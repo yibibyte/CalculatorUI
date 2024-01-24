c968d0434b119b361dcef1a2639666e845c2a064
yibibyte <gnia@artcollege.ru>
2024-01-24 15:28:56 +0300

    Initial Calculator UI

---
+++ src/main/java/com/example/calculator/CalculatorApp.java
@@ -0,0 +1,33 @@
+package com.example.calculator;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.util.Objects;
+
+/**
+ * @author I.G.V.
+ * @version 1.0.0.1
+ */
+public class CalculatorApp extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("calculatorUI.fxml")));
+
+        Scene scene = new Scene(root);
+
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
---
+++ src/main/java/com/example/calculator/CalculatorUIController.java
@@ -0,0 +1,103 @@
+package com.example.calculator;
+
+import javafx.fxml.Initializable;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+public class CalculatorUIController implements Initializable {
+
+        Double temp = 0.0, sum = 0.0;
+        boolean isOperatorPressed;
+        String operatorPressed = "";
+
+        @FXML
+        TextField outputTF;
+
+        /**
+         * Initializes the controller class.
+         */
+        @Override
+        public void initialize(URL url, ResourceBundle rb) {
+            outputTF.textProperty().addListener(new ChangeListener<String>() {
+                @Override
+                public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
+                    if (!newValue.matches("\\d*([\\.]\\d*)?")) {
+                        outputTF.setText(oldValue);
+                    }
+                }
+            });
+        }
+
+        @FXML
+        private void onNumberClick(ActionEvent event) {
+            if(event.getSource() instanceof Button) {
+                Button btn = (Button)event.getSource();
+                if(isOperatorPressed) {
+                    outputTF.setText(btn.getText().trim());
+                } else {
+                    outputTF.setText(outputTF.getText().trim() + btn.getText().trim());
+                }
+                isOperatorPressed = false;
+            }
+        }
+
+        @FXML
+        private void onOperatorClick(ActionEvent event) {
+            if(event.getSource() instanceof Button) {
+                Button btn = (Button)event.getSource();
+                if (!outputTF.getText().isEmpty()) {
+                    temp = Double.valueOf(outputTF.getText());
+                    if (btn.getText().equals("%")) {
+                        temp = sum * temp / 100;
+                    }
+                    switch (operatorPressed) {
+                        case "/":
+                            sum /= temp;
+                            break;
+                        case "X":
+                            sum *= temp;
+                            break;
+                        case "+":
+                            sum += temp;
+                            break;
+                        case "-":
+                            sum -= temp;
+                            break;
+                        default:
+                            sum = temp;
+                    }
+                }
+
+                if (btn.getText().equals("=") || btn.getText().equals("%")) {
+                    outputTF.setText(String.valueOf(sum));
+                    operatorPressed = "";
+                } else {
+                    outputTF.setText("");
+                    operatorPressed = btn.getText().trim();
+                }
+                isOperatorPressed = true;
+            }
+        }
+
+        @FXML
+        private void onDELClick(ActionEvent event) {
+            if(outputTF.getText().length() > 0) {
+                outputTF.setText(outputTF.getText(0, outputTF.getText().length() - 1));
+            }
+        }
+
+        @FXML
+        private void onCEClick(ActionEvent event) {
+            outputTF.setText("");
+            temp = 0.0;
+            sum = 0.0;
+            isOperatorPressed = false;
+            operatorPressed = "";
+        }
+    }
+
